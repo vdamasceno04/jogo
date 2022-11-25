a0294c3514b5b503bfe6804e7acf33103af7755b
victordamascenooliveira@gmail.com <victordamascenooliveira@gmail.com>
2022-11-25 20:35:01 -0300

    posicao gosma ajustada

--- gosma.cpp
+++ gosma.cpp
@@ -3,7 +3,7 @@
 
 Gosma::Gosma(sf::Vector2f pos, int ven) : Entidades::Entidade(pos) {
 	id = gosma;
-	setPosicao(sf::Vector2f(pos.x +40, pos.y+60));
+	setPosicao(sf::Vector2f(pos.x +20, pos.y+60));
 	setEscala(sf::Vector2f(2, 2));
 	body.setSize(sf::Vector2f(4, 8));
 	veneno = ven;
@@ -29,13 +29,13 @@ void Gosma::setCaracol(Caracol* pCar) {
 void Gosma::colidir(Entidade* p, float intersec_x, float intersec_y) {
 	if (p->getId() == 3 || p->getId() == 6 || p->getId() == 5 || p->getId() == 4 || p->getId() == 7) {
 		resolverColisao(p, intersec_x, intersec_y);
-		setPosicao(sf::Vector2f(pCaracol->getPosicao().x + 40, pCaracol->getPosicao().y + 60));
+		setPosicao(sf::Vector2f(pCaracol->getPosicao().x + 20, pCaracol->getPosicao().y + 60));
 		inicializar();
 		
 	}
 	else if (p->getId() == 1 || p->getId() == 2) {
 		resolverColisao(p, intersec_x, intersec_y);
-		setPosicao(sf::Vector2f(pCaracol->getPosicao().x + 40, pCaracol->getPosicao().y + 60));
+		setPosicao(sf::Vector2f(pCaracol->getPosicao().x + 20, pCaracol->getPosicao().y + 60));
 		inicializar();
 		efeito(p);
 		
