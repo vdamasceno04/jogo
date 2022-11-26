a68a6b83367abb0a5318870ae3441400ab33006c
victordamascenooliveira@gmail.com <victordamascenooliveira@gmail.com>
2022-11-26 16:24:32 -0300

    agora o elemento é aninhado

--- Lista.h
+++ Lista.h
@@ -2,75 +2,88 @@
 #include <iostream>
 #include"Elemento.h"
 
-namespace Listas {
-	template <class TE>  class Lista
-	{
+template <class TE>  class Lista
+{
+public:
+	template <class TE> class Elemento {
 	private:
-		Elemento<TE>* pPrimeiro;
-		Elemento<TE>* pUltimo;
-		int len;
+		Elemento<TE>* pProx;
+		TE* item;
 	public:
-		Lista();
-		~Lista();
-		int getLen() { return len; }
+		Elemento() { pProx = nullptr; item = nullptr; }
+		~Elemento() {}
+		void setpProx(Elemento<TE>* pProx) { this->pProx = pProx; }
+		void setItem(TE* item) { this->item = item; }
+		Elemento<TE>* getpProx() { return pProx; }
+		TE* getItem() { return item; }
 
-		TE* getItem(int posicao) {
-			Elemento<TE>* temp = pPrimeiro;
-			if (posicao == 0)
-				return temp->getItem();
-			for (int i = 0; i < posicao; i++)
-				temp = temp->getpProx();
-			return temp->getItem();
-		}
-
-		void push(TE* item) {
-			if (pPrimeiro == nullptr) {
-				pPrimeiro = new Elemento<TE>;
-				pPrimeiro->setItem(item);
-				pUltimo = pPrimeiro;
-			}
-			else {
-				Elemento<TE>* temp = new Elemento<TE>;
-				temp->setItem(item);
-				pUltimo->setpProx(temp);
-				pUltimo = temp;
-			}
-			len++;
-		}
-
-		void pop(TE* item) {
-			Elemento<TE>* temp = pPrimeiro;
-			Elemento<TE>* tempAnt = nullptr;
-			while (temp->getItem() != item) {
-				tempAnt = temp;
-				temp = temp->getpProx();
-			}
-			if (temp == pPrimeiro) {
-				pPrimeiro = temp->getpProx();
-			}
-			else if (temp == pUltimo) {
-				tempAnt->setpProx(nullptr);
-				pUltimo = tempAnt;
-			}
-			else {
-				tempAnt->setpProx(temp->getpProx());
-			}
-			delete temp;
-			len--;
-		}
 	};
 
-	template<class TE>
-	inline Lista<TE>::Lista() {
-		pPrimeiro = NULL;
-		pUltimo = NULL;
-		len = 0;
+private:
+	Elemento<TE>* pPrimeiro;
+	Elemento<TE>* pUltimo;
+	int len;
+public:
+	Lista();
+	~Lista();
+	int getLen() { return len; }
+
+	TE* getItem(int posicao) {
+		Elemento<TE>* temp = pPrimeiro;
+		if (posicao == 0)
+			return temp->getItem();
+		for (int i = 0; i < posicao; i++)
+			temp = temp->getpProx();
+		return temp->getItem();
 	}
 
-	template<class TE>
-	inline Lista<TE>::~Lista() {
-		pPrimeiro = NULL;
-		pUltimo = NULL;
-		len = 0;
+	void push(TE* item) {
+		if (pPrimeiro == nullptr) {
+			pPrimeiro = new Elemento<TE>;
+			pPrimeiro->setItem(item);
+			pUltimo = pPrimeiro;
+		}
+		else {
+			Elemento<TE>* temp = new Elemento<TE>;
+			temp->setItem(item);
+			pUltimo->setpProx(temp);
+			pUltimo = temp;
+		}
+		len++;
 	}
+
+	void pop(TE* item) {
+		Elemento<TE>* temp = pPrimeiro;
+		Elemento<TE>* tempAnt = nullptr;
+		while (temp->getItem() != item) {
+			tempAnt = temp;
+			temp = temp->getpProx();
+		}
+		if (temp == pPrimeiro) {
+			pPrimeiro = temp->getpProx();
+		}
+		else if (temp == pUltimo) {
+			tempAnt->setpProx(nullptr);
+			pUltimo = tempAnt;
+		}
+		else {
+			tempAnt->setpProx(temp->getpProx());
+		}
+		delete temp;
+		len--;
+	}
+};
+
+template<class TE>
+inline Lista<TE>::Lista() {
+	pPrimeiro = NULL;
+	pUltimo = NULL;
+	len = 0;
+}
+
+template<class TE>
+inline Lista<TE>::~Lista() {
+	pPrimeiro = NULL;
+	pUltimo = NULL;
+	len = 0;
 }
